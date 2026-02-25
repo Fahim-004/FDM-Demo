731ebe51368da5ec639adf01de913f4df6d8b509
permansharma7349-code <permansharma7349@gmail.com>
2026-02-25 18:28:19 +0530

    trial commits

---
+++ productdemo/src/main/java/com/example/productdemo/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.productdemo.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.productdemo.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    List<Product> findByNameContainingIgnoreCaseOrCategoryContainingIgnoreCase(
+            String name, String category);
+}
